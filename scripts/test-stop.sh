256215bab03e708c65b02b9ef7120ea40d983eb1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-06-15 13:58:30 +0200

    add echo

--- scripts/test-stop.sh
+++ scripts/test-stop.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "Starting server."
 python -m schul_cloud_search_tests.proxy 8081 /endpoint/ http://localhost:8080/v1/search/ &
 PID=$!
+echo "..."
 sleep 1
 
+echo "Requesting stop."
 python -m schul_cloud_search_tests.stop 8081
+echo "..."
 sleep 1
 
+echo "Waiting for server..."
 ! wait $PID
-
+echo "Done"
