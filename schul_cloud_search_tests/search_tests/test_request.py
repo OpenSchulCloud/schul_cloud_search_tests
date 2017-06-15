eaa7579ec75e9c5e3b03d06e968d5b62239eddab
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-06-15 14:30:33 +0200

    test q is there

--- schul_cloud_search_tests/search_tests/test_request.py
+++ schul_cloud_search_tests/search_tests/test_request.py
@@ -4,7 +4,9 @@ from pytest import mark
 @mark.request
 def test_request_has_query(request):
     """Test that the query includes a "q"."""
-    assert request.query["q"]
+    assert "q" in request.query
+
+
 
 
 
