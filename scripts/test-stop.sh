a2b15bdd8d18f49d8ed19767a764b57b408b5bfb
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-06-15 13:47:49 +0200

    remove echo

--- scripts/test-stop.sh
+++ scripts/test-stop.sh
@@ -2,14 +2,11 @@
 set -e
 
 python -m schul_cloud_search_tests.proxy 8081 /endpoint/ http://localhost:8080/v1/search/ &
-echo 1
 PID=$!
-echo 2
 sleep 1
-echo 3
+
 python -m schul_cloud_search_tests.stop 8081
-echo 4
 sleep 1
-echo 5
+
 ! wait $PID
-echo 6
+
