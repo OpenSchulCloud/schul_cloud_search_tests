e112d6d7b2d8314462d793b5edae359eb444f087
Nicco Kunzmann <niccokunzmann@rambler.ru>
2017-07-20 15:52:32 -0700

    testing stop script

--- scripts/test-stop.sh
+++ scripts/test-stop.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Starting server."
-python -m schul_cloud_search_tests.proxy
+python -m schul_cloud_search_tests.proxy &
 PID=$!
 echo "..."
-sleep 1
+sleep 10
 
 echo "Requesting stop."
 python -m schul_cloud_search_tests.stop 8081
